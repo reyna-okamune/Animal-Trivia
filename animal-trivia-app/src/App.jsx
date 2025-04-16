e415dabc2e41a3e32751e1b313ec14b8150b88b8
rokamune <reynaoaguirre@gmail.com>
2025-04-16 11:18:48 -0700

    comments for data structure

--- animal-trivia-app/src/App.jsx
+++ animal-trivia-app/src/App.jsx
@@ -7,17 +7,28 @@ function App() {
   const [triviaQuestions, setTriviaQuestions] = useState([])
 
   /* FETCHING OPEN TRIVIA API DATA */
-
   async function fetchTriviaData() {
     const response = await axios.get("https://opentdb.com/api.php?amount=10&category=27&type=multiple")
     setTriviaQuestions(response.data.results)
 
   }
 
-  // on mount fetch the api data
   useEffect(() => {
     fetchTriviaData();
   }, [])
+
+
+  /* ORGANIZING DATA */
+  /* 
+      strucutre: array of objects 
+      example: 
+        category: "Animals"
+        correct_answer: "Tail"
+        difficulty: "hard"
+        incorrect_answers (3): ['Head', 'Teeth', 'Feet']
+        question: "Unlike on most salamanders, this part of a newt is flat?"
+        type: "multiple"
+  */
 
   /* DEBUGGING */
   console.log(triviaQuestions)
